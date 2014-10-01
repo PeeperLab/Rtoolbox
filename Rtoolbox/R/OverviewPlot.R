7b1b1eee48a4ca1295faa0640983416561ed80f3
Thomas Kuilman <t.kuilman@Thomas-Kuilmans-Macbook.local>
2014-10-01 21:31:35 +0200

    Added label to chromosomes

--- Rtoolbox/R/OverviewPlot.R
+++ Rtoolbox/R/OverviewPlot.R
@@ -1,4 +1,4 @@
-OverviewPlot <- function(DNAcopy.object, samples, range.CNA = c(-2, 2), color.palette = colorRampPalette(c("blue", "white", "white", "red"))(49)) {
+OverviewPlot <- function(DNAcopy.object, samples, range.CNA = c(-2, 2), color.palette = colorRampPalette(c("blue", "white", "red"))(49)) {
 	
 	## Use all samples by default
 	if(missing(samples)) {
@@ -18,7 +18,8 @@ OverviewPlot <- function(DNAcopy.object, samples, range.CNA = c(-2, 2), color.pa
 	# Names are changed by dcast according to level order -> change order levels (!)
 	order.samples <- unique(DNAcopy.object$output$ID)
 	DNAcopy.object$output$ID <- factor(DNAcopy.object$output$ID, levels = order.samples)
-	DNAcopy.object.cast <- reshape2::dcast(data = DNAcopy.object$output, formula = ID + chrom + loc.start + loc.end + num.mark + seg.mean ~ ID, value.var = "num.mark")
+	DNAcopy.object.cast <- reshape2::dcast(data = DNAcopy.object$output, formula = ID + chrom + loc.start + loc.end + num.mark + seg.mean ~ ID
+		, value.var = "num.mark")
 	DNAcopy.object.cast[is.na(DNAcopy.object.cast)] <- 0
 	
 	## Calculate number of samples
@@ -32,11 +33,14 @@ OverviewPlot <- function(DNAcopy.object, samples, range.CNA = c(-2, 2), color.pa
 	
 	## Create overviewPlot
 	par(mfrow = c(1, 2 + sample.number), mar = c(7, 0, 1, 0))
-	barplot(as.matrix(DNAcopy.object.cast.aggregate[, "num.mark"]), col = c("black", "white"), border = NA, yaxt = "n", xlim = c(0, 0.1), width = 0.1)
+	barplot(matrix(DNAcopy.object.cast.aggregate[, "num.mark"], dimnames = list(NULL, "Chr")), col = c("black", "white"), border = NA
+		, yaxt = "n", xlim = c(0, 0.1), width = 0.1, las = 2)
 	for(i in 1:sample.number) {
-		barplot(as.matrix(DNAcopy.object.cast[, 6 + i, drop = FALSE]), col = color.palette[1 + (DNAcopy.object.cast$seg.mean - range.CNA[1]) / range.factor * 48], border = NA
+		barplot(as.matrix(DNAcopy.object.cast[, 6 + i, drop = FALSE])
+			, col = color.palette[1 + (DNAcopy.object.cast$seg.mean - range.CNA[1]) / range.factor * 48], border = NA
 			, yaxt = "n", xlim = c(0, 0.1), width = 0.1, las = 2)
 	}
-	barplot(matrix(rep(1, length(color.palette)), ncol = 1, dimnames = list(NULL, paste(range.CNA, collapse = " to "))), col = color.palette, border = NA, yaxt = "n", beside = FALSE, xlim = c(0, 0.1), width = 0.1, las = 2)
+	barplot(matrix(rep(1, length(color.palette)), ncol = 1, dimnames = list(NULL, paste(range.CNA, collapse = " to ")))
+		, col = color.palette, border = NA, yaxt = "n", beside = FALSE, xlim = c(0, 0.1), width = 0.1, las = 2)
 	
 }
